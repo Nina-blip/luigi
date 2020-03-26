a4bd7dcb413bc136bac467e875e614199ab38424
Nina <nina.vanhoof@vdabcampus.be>
2020-03-26 10:11:31 +0100

    Stateless protocol

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("pizzas")
@@ -16,6 +18,17 @@ class PizzaController {
     private final Pizza[] pizzas = {new Pizza(1, "Proscuitto", BigDecimal.valueOf(4), true),
             new Pizza(2, "Margherita", BigDecimal.valueOf(5), false),
             new Pizza(3, "Calzone", BigDecimal.valueOf(4), false)};
+
+    private List<BigDecimal> uniekePrijzen() {
+        return Arrays.stream(pizzas).map(pizza -> pizza.getPrijs())
+                .distinct().sorted().collect(Collectors.toList());
+    }
+
+    private List<Pizza> pizzasMetPrijs(BigDecimal prijs) {
+        return Arrays.stream(pizzas)
+                .filter(pizza -> pizza.getPrijs().compareTo(prijs) == 0)
+                .collect(Collectors.toList());
+    }
 
     @GetMapping
     public ModelAndView pizzas() {
@@ -27,6 +40,18 @@ class PizzaController {
         ModelAndView modelAndView = new ModelAndView("pizza");
         Arrays.stream(pizzas).filter(pizza -> pizza.getId()== id).findFirst()
                 .ifPresent(pizza -> modelAndView.addObject(pizza));
+        return modelAndView;
+    }
+
+    @GetMapping("prijzen")
+    public ModelAndView prijzen() {
+        return new ModelAndView("prijzen", "prijzen", uniekePrijzen());
+    }
+
+    @GetMapping("prijzen/{prijs}")
+    public ModelAndView pizzasMetEenPrijs(@PathVariable BigDecimal prijs) {
+        ModelAndView modelAndView = new ModelAndView("prijzen","pizzas",pizzasMetPrijs(prijs));
+        modelAndView.addObject("prijzen", uniekePrijzen());
         return modelAndView;
     }
 }
