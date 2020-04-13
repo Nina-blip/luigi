1d6670d9fadf86ffac0d94f2843b00109ab250e5
Nina <nina.vanhoof@vdabcampus.be>
2020-04-13 09:30:45 +0200

    Forms

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -2,11 +2,13 @@ package be.vdab.luigi.controllers;
 
 import be.vdab.luigi.domain.Pizza;
 import be.vdab.luigi.exceptions.KoersClientException;
+import be.vdab.luigi.forms.VanTotPrijsForm;
 import be.vdab.luigi.services.EuroService;
 import be.vdab.luigi.services.PizzaService;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +39,7 @@ class PizzaController {
 
     private final EuroService euroService;
     private final PizzaService pizzaService;
-    private final Logger logger= LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     PizzaController(EuroService euroService, PizzaService pizzaService) {
         this.euroService = euroService;
@@ -75,5 +77,19 @@ class PizzaController {
         ModelAndView modelAndView = new ModelAndView("prijzen", "pizzas", pizzaService.findByPrijs(prijs));
         modelAndView.addObject("prijzen", pizzaService.findUniekePrijzen());
         return modelAndView;
+    }
+
+    @GetMapping("vantotprijs/form")
+    public ModelAndView vanTotPrijsForm() {
+        return new ModelAndView("vantotprijs").addObject(new VanTotPrijsForm(null, null));
+    }
+
+    @GetMapping("vantotprijs")
+    public ModelAndView vanTotPrijs(VanTotPrijsForm form, Errors errors) {
+        ModelAndView modelAndView = new ModelAndView("vantotprijs");
+        if (errors.hasErrors()) {
+            return modelAndView;
+        }
+        return modelAndView.addObject("pizzas", pizzaService.findByPrijsBetween(form.getVan(), form.getTot()));
     }
 }
